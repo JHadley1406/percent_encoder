20bff03cac3937b0b458598a3b0f20cc0b200d06
Josiah Hadley <josiah.hadley@gmail.com>
2025-11-19 15:43:44 -0800

    initial commit

---
+++ encoder.py
@@ -0,0 +1,47 @@
+encoding_dict = {
+    " ": "%20",
+    "!": "%21",
+    "\"": "%22",
+    "#": "%23",
+    "$": "%24",
+    "%": "%25",
+    "&": "%26",
+    "'": "%27",
+    "(": "%28",
+    ")": "%29",
+    "*": "%2A",
+    "+": "%2B",
+    ",": "%2C",
+    "-": "%2D",
+    ".": "%2E",
+    "/": "%2F",
+    ":": "%3A",
+    ";": "%3B",
+    "<": "%3C",
+    "=": "%3D",
+    ">": "%3E",
+    "?": "%3F",
+    "@": "%40",
+    "[": "%5B",
+    "\\": "%5C",
+    "]": "%5D",
+    "^": "%5E",
+    "_": "%5F",
+    "`": "%60",
+    "{": "%7B",
+    "|": "%7C",
+    "}": "%7D",
+    "~": "%7E"
+}
+
+def encode(input: str) -> str:
+    encoded_chars = []
+    for char in input:
+        encoded_chars.append(encoding_dict.get(char, char))
+    return "".join(encoded_chars)
+
+if __name__ == "__main__":
+    input_string = input("Enter string to encode: ")
+    encoded_string = encode(input_string)
+    print("Encoded string: ")
+    print(encoded_string)
